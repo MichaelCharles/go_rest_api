dca8146d0f39ed35eea8b034f638be31ed00370d
Michael Charles Aubrey <michael@akita-itlabo.com>
2021-11-06 18:24:16 +0900

    feat(add routing): adds routing and a basic healthcheck endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/mcaubrey/go_rest_api/internal/transport/http"
+)
 
 // App - the struct which contains things like pointers to database connections
 type App struct {
@@ -9,6 +14,14 @@ type App struct {
 // Run - sets up our application.
 func (app *App) Run() error {
 	fmt.Println("Setting up our app...")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mcaubrey/go_rest_api
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
