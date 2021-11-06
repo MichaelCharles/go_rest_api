dc8e801d46cfb9222c40680e6d5550d3795f1151
Michael Charles Aubrey <michael@akita-itlabo.com>
2021-11-06 18:08:42 +0900

    feat(structure our application): add some extra structure to the application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things like pointers to database connections
+type App struct {
+}
+
+// Run - sets up our application.
+func (app *App) Run() error {
+	fmt.Println("Setting up our app...")
+	return nil
+}
+
 func main() {
 	fmt.Println("This was a triumph.")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
